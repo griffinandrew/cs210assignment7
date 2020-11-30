b99570e793a1cd5b61cb1b7b67219f6783a870b6
Griffin Heyrich <gheyrich@csa1.bu.edu>
2020-11-30 11:55:52 -0500

    discussion code

--- csim.c
+++ csim.c
@@ -1,7 +1,109 @@
+//Griffin Heyrich
+//u42992446
 #include "cachelab.h"
+#include <stdio.h>
+#include<stdlib.h>
+#include <getopt.h>
 
-int main()
+
+int sets;
+int assoc;
+int bytes;
+
+
+
+typedef struct Block{
+    int tag;
+    int valid_bit;
+    int lri; //time stamp
+} block;
+
+typedef struct Set{
+    unsigned int index;
+    block *my_set; //array of sets in struct
+} set;
+
+set *my_cache;
+
+
+void create_cache(int set_bits, int assoc, int offset_bits){
+    sets = 1 << set_bits; //left shift same as mult by 2
+    assoc = assoc;
+    bytes = 1 << offset_bits;
+
+    printf("%d, %d, %d\n", sets, assoc, bytes);
+    printf("my_cache: %p\n", my_cache);
+
+    my_cache = (set *)malloc(sets* sizeof(set) ); //alloc space for cache
+    printf("my_cache: %p\n", my_cache);
+
+
+    int i =0;
+    int j =0;
+    for (i= 0; i < sets; i++)
+    {
+        my_cache[i].index = i;
+        my_cache[i].my_set = (block *) malloc(assoc * sizeof(block)); //allocate my set within eac hset
+
+
+        for (j = 0; j < assoc; j ++) //each block within set intialize there starting information
+        {
+            my_cache[i].my_set[j].tag = -1;
+            my_cache[i].my_set[j].valid_bit = -1;
+            my_cache[i].my_set[j].lri = -1;
+        }
+    }
+}
+
+
+
+
+
+int main(int argc, char **argv)  //int is number of args char is strings part of that arg list (list of strings bascvially) 
 {
+    int set_bits = 0;
+    int associativity =0;
+    int offset_bits = 0;
+    char *trace_file = NULL;
+
+
+    char opt;
+
+    while(1)
+    {
+
+        opt = getopt(argc, argv, "vhs:E:b:t:"); //looks at argv and tries to match it with one of those things if found opt gives letter that first found
+        
+
+        if (opt == -1){
+            break;
+        }
+
+        switch(opt){
+            case 's':
+                set_bits = atoi(optarg); //opt agrv var created wtih get opt, this is string, atoi turns strong to int if possible
+                break;
+            case 'E':
+                associativity = atoi(optarg);
+                break;
+            case 'b':
+                offset_bits = atoi(optarg);
+                break;
+            case 't':
+                trace_file = optarg;
+                break;
+            default:
+                break;
+        }
+
+    }
+
+
+
+    printf("%d, %d, %d, %s\n", set_bits, associativity, offset_bits, trace_file);
+
+    create_cache(set_bits, associativity, offset_bits);
+
     printSummary(0, 0, 0);
     return 0;
 }
